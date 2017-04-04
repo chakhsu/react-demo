6b1f34c325f2a2b6f126fc8a454ef3ee6ddcce23
Chakhsu Lau <zexu.liu@live.cn>
2017-04-04 16:18:43 +0800

    updte

--- app/index.js
+++ app/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import Profile from './components/Profile.jsx';
 
 const props = {
-  name: 'Chakhsu Lau',
-  age: 23
+  name: 'Chakhsu Lau'
 };
 
 ReactDOM.render(<Profile {...props} />, document.getElementById('root'));
